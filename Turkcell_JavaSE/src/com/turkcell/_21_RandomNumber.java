1664db809bf72792c742f7ec3cef86cea0cf2b9b
Beytullah ZOR <beytullahzor7@gmail.com>
2022-06-23 10:44:28 +0300

    Updated

--- Turkcell_JavaSE/src/com/turkcell/_21_RandomNumber.java
+++ Turkcell_JavaSE/src/com/turkcell/_21_RandomNumber.java
@@ -3,12 +3,15 @@ package com.turkcell;
 public class _21_RandomNumber {
 	
 	public static void main(String[] args) {
-		// 1-10 arasında 5 tane random sayi üretelim (random-for)
+		// 1-10 arasinda 5 tane random sayi iretelim (random-for)
+		
+		double sum = 0;
 		
 		for (int i = 0; i < 5; i++) {
 			double number = Math.random() * 10;
 			System.out.println(i + ". number " + number);
+			sum += number;
 		}
-		
+		System.out.println("Sum : " + sum);
 	}
 }
