585826fe79533fc25d9a6c167e9c10a65117425a
Beytullahzor7 <beytullahzor7@gmail.com>
2022-06-30 10:34:05 +0300

    I/O

---
+++ Turkcell_JavaSE/src/com/turkcell/_64_FileIO.java
@@ -0,0 +1,23 @@
+package com.turkcell;
+
+public class _64_FileIO {
+	
+	// IO = Input / Output
+	// file = Db olmadigi durumlarda verileri saklamak istedigimizde kullanabiliriz
+	// 8 bit = 1 byte
+	
+	// 8 bit veri göndermek / almak
+	// FileInputStream (8 bit veri alacak)
+	// FileOutputStream (8 bit veri verecek)
+	
+	// FileWriter / FileReader
+	
+	// Buffered:Tamponlama
+	// Unuttmaaaa:
+	// try-catch-finally kullanalým
+	// Java escape character ==> \
+	// Windows'ta ==> \
+	// Linux'ta ==> /
+	
+	public static final String MY_PATH = "C:\\turkcell\\data.txt";
+}
